e182a125bf024ba169b7c7b583a934bf4bbf0645
Paul Irish <paul.irish@gmail.com>
2016-08-29 16:44:31 -0700

    conf

---
+++ docs/config.js
@@ -0,0 +1,9 @@
+var config = {
+    scopes: [
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'https://www.googleapis.com/auth/drive.install',
+        'https://www.googleapis.com/auth/drive.file',
+        'profile'
+    ],
+    clientId: '963139201929-vdv8qjb57uevrip1ab5325ios43qju4l.apps.googleusercontent.com	'
+}
