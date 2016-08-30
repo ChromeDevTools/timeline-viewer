8cea418f1bf2b51613185d99217608786c6278a4
Paul Irish <paul.irish@gmail.com>
2016-08-29 22:18:12 -0700

    bit of a refactor for smarter devtools integration.

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,17 @@
+var params = new URL(location.href).searchParams;
+var timelineURL = params.get('loadTimelineFromURL');
+var timelineId;
+
+// if timelineURL isn't a real URL, then we'll save it to an ID
+try {
+    new URL(timelineURL);
+} catch (e) {
+    timelineId = timelineURL
+}
+
 var authBtn = document.getElementById('auth');
 
+// called when apis.google script is done.
 function checkAuth() {
     gapi.auth.authorize({
         'client_id': config.clientId,
@@ -27,37 +39,67 @@ function handleAuthClick(event) {
     return false;
 }
 
+var driveAPIloaded;
 function loadDriveApi() {
-    gapi.client.load('drive', 'v2', openFile);
+    driveAPIloaded = new Promise((resolve, reject) => {
+        gapi.client.load('drive', 'v2', resolve);    
+    });
 }   
 
-function openFile () {
-    if (location.href.split('?').length < 2) {
+// This is defined in devtools' Runtime
+_loadResourcePromise = loadResourcePromise
+loadResourcePromise = function(url){ 
+    // fallthrough
+    if (url !== timelineId) return _loadResourcePromise(...arguments);
+    // special handling for us..
+    return driveAPIloaded.then(_ => {
+        return new Promise((resolve, reject) => {
+            openFile(resolve, reject);
+        })
+    })
+}
+
+function init(){
+    if (!timelineURL) {
         document.getElementById('howto').style.display = 'block';
         return;
     }
+
     // show loading message..
     document.getElementById('opening').style.display = 'inline';
-    // start devtools
-    Runtime.startApplication("inspector");
     
-    var id = location.href.split('?')[1].split('=')[1];
+    // start devtools. 
+    Runtime.startApplication("inspector");
+}
+
+function openFile (resolve, reject) {
+    // if there's no timelineId then let's skip all this drive API stuff.
+    if (!timelineId) return;
+
     var request = gapi.client.drive.files.get({
-        fileId: id
+        fileId: timelineId
     });
     request.execute(function (response) {
         var url = response.downloadUrl + '&alt=media'; // forces file contents in response body.
         downloadFile(url, function(payload){  
-            var fileparts = [payload];
-            var traceblob = new Blob(fileparts, {type : 'application/json'});
 
-            (function loopy() {
-              if (window.WebInspector && WebInspector.panels && WebInspector.panels.timeline && WebInspector.panels.timeline._loadFromFile){
-                WebInspector.panels.timeline._loadFromFile(traceblob);
-              } else {
-                setTimeout(loopy,50);
-              }
-            })();
+            return resolve(payload);
+            
+            // ignore that rubbish
+            // var fileparts = [payload];
+            // var traceblob = new Blob(fileparts, {type : 'application/json'});
+
+            // (function loopy() {
+            //   if (window.WebInspector && WebInspector.panels && WebInspector.panels.timeline && WebInspector.panels.timeline._loadFromFile){
+                
+
+            //     WebInspector.panels.timeline._loadFromFile(traceblob);
+
+
+            //   } else {
+            //     setTimeout(loopy,50);
+            //   }
+            // })();
         });
     })
 }
@@ -79,3 +121,5 @@ function downloadFile(url, callback) {
     callback(null);
   }
 }
+
+init();
