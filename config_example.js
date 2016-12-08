c68b8a316760a2bbbf63effbafdb49aeb7baccd9
Paul Irish <paul.irish@gmail.com>
2016-12-07 16:34:00 -0800

    remove all dependencies. dont need em

--- config_example.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    scopes: [
-        'https://www.googleapis.com/auth/drive.metadata.readonly',
-        'https://www.googleapis.com/auth/drive.install',
-        'https://www.googleapis.com/auth/drive.file',
-        'profile'
-    ],
-    clientId: 'YOUR-CLIENT-ID-HERE'
-}
