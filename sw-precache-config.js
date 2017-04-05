e58227633142b2e476be6caf55c71b63195d9732
Artem Denysov <denysov.artem@gmail.com>
2017-04-05 21:28:33 +0300

    timeline-viewer now works offline via service worker (#9)
    * use latest sw-precache version
    * add network status message

---
+++ sw-precache-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  staticFileGlobs: [
+    'docs/**.css',
+    'docs/**.html',
+    'docs/Images/**.*',
+    'docs/**.js'
+  ],
+  runtimeCaching: [{
+    // exclude googleapis, analitics, drive requests
+    urlPattern: /^((?!(googleapis|analytics|googleusercontent)).)*$/,
+    handler: 'networkFirst'
+  }],
+  stripPrefix: 'docs/',
+  verbose: true
+};
