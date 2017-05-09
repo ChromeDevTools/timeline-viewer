4bff55f32002152e91263ab713e788fc96685180
Artem Denysov <denysov.artem@gmail.com>
2017-05-10 01:50:06 +0300

    update sw

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["Images/toolbarButtonGlyphs.png","d71330d9fb3d9263767c817a5b341fd3"],["google8b1867cdabba50ce.html","6f460bcd31c3a7f9d1c5cb2da7b6b3d1"],["index.html","888c5d77b31bccd1756ba3870c958299"],["styles.css","ca513056112ef516b336233a58083405"],["sync_view.js","bc80470f6213a6b627a7dd20d890766d"],["timeline_viewer.js","ade2369191e5f2c3eea91146b382c253"],["timeline_viewer_config.js","abb2d272401849734ff25be2ba212904"]];
+var precacheConfig = [["Images/toolbarButtonGlyphs.png","d71330d9fb3d9263767c817a5b341fd3"],["google8b1867cdabba50ce.html","6f460bcd31c3a7f9d1c5cb2da7b6b3d1"],["index.html","888c5d77b31bccd1756ba3870c958299"],["styles.css","ca513056112ef516b336233a58083405"],["sync_view.js","e519d702d7b919dcdc82c615a87b6de2"],["timeline_viewer.js","8f4e389e595abf3e9fdc700b8c4c1198"],["timeline_viewer_config.js","abb2d272401849734ff25be2ba212904"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
