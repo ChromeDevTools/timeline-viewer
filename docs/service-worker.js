27b1fbbfd5deac578bdb6ab24f01b1bf84839779
Paul Irish <paul.irish@gmail.com>
2017-05-09 17:14:54 -0700

    lazy monkeypatch.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["Images/toolbarButtonGlyphs.png","d71330d9fb3d9263767c817a5b341fd3"],["google8b1867cdabba50ce.html","6f460bcd31c3a7f9d1c5cb2da7b6b3d1"],["index.html","888c5d77b31bccd1756ba3870c958299"],["styles.css","ca513056112ef516b336233a58083405"],["sync_view.js","eb664f24771baa4316c1f5af2044816a"],["timeline_viewer.js","51761d7ae16723a492e133605bede32e"],["timeline_viewer_config.js","abb2d272401849734ff25be2ba212904"]];
+var precacheConfig = [["Images/toolbarButtonGlyphs.png","d71330d9fb3d9263767c817a5b341fd3"],["google8b1867cdabba50ce.html","6f460bcd31c3a7f9d1c5cb2da7b6b3d1"],["index.html","888c5d77b31bccd1756ba3870c958299"],["styles.css","ca513056112ef516b336233a58083405"],["sync_view.js","9c472dd4028e381a029d14aec4a2e7f5"],["timeline_viewer.js","51761d7ae16723a492e133605bede32e"],["timeline_viewer_config.js","abb2d272401849734ff25be2ba212904"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
